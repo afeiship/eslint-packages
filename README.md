8a511154040575aa0c5b20e7bde7b19d966cba64
afeiship <1290657123@qq.com>
2020-05-19 14:12:48 +0800

    feat: intial

---
+++ README.md
@@ -0,0 +1,7 @@
+# ushell-module-brew
+> Ushell module for mac brew.
+
+## installation
+```shell
+npm i -S @feizheng/ushell-module-brew
+```
---
+++ index.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles;
+
+unset ROOT_PATH;
