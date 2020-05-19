444314f7be4b89b7decd2577c19217f0308b6470
afeiship <1290657123@qq.com>
2020-05-19 22:47:22 +0800

    feat: update init

--- initialize.sh
+++ initialize.sh
@@ -2,6 +2,24 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+# 替换方式为：
+# 替换brew.git:
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.cloud.tencent.com/homebrew/brew.git
 
+# 替换homebrew-core.git:
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.cloud.tencent.com/homebrew/homebrew-core.git
+
+
+# 更新 bottles源
+#### ----  对于bash用户：---- ####
+
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles' >> ~/.bash_profile
+source ~/.bash_profile
+
+#### ---- 对于zsh用户  --- ####
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles' >> ~/.zshrc
+source ~/.zshrc
 
 unset ROOT_PATH;
