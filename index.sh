741fdc001c67e52b0880d2fcb7ca32a16bc6ef42
afeiship <1290657123@qq.com>
2020-10-21 13:56:13 +0800

    feat: add bws

--- index.sh
+++ index.sh
@@ -4,4 +4,12 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles;
 
+# aliases
+
+alias bws-list=brew services list
+alias bws-start=brew services start
+alias bws-stop=brew services stop
+alias bws-restart=brew services restart
+alias bws-cleanup=brew services cleanup
+
 unset ROOT_PATH;
